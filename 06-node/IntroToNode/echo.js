c2e6f6113ad0cec3e1ad427dd88e035d0a1991ca
Fabrício Silva <fabriciofmsilva@gmail.com>
2019-04-25 22:19:39 -0300

    Node Echo Exercise

---
+++ 06-node/IntroToNode/echo.js
@@ -0,0 +1,11 @@
+function echo(text, times) {
+  for (let i = 0; i < times; i++) {
+    console.log(text);
+  }
+}
+
+echo('Echo!!!', 10);
+echo('Tater Tots', 3);
+
+echo('Bla', 2);
+echo('Foo', 4);
