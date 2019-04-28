be26d6cbe77d85c6f2e192149837a288fd8d6f27
Fabrício Silva <fabriciofmsilva@gmail.com>
2019-04-27 22:27:27 -0300

    Route Params

--- 06-node/FirstExpressApp/app.js
+++ 06-node/FirstExpressApp/app.js
@@ -13,6 +13,18 @@ app.get('/dog', function(req, res) {
   res.send('Woof!');
 });
 
+app.get('/r/:subredditName', function(req, res) {
+  res.send(`Welcome to the ${req.params.subredditName} subreddit!`);
+});
+
+app.get('/r/:subredditName/comments/:id/:title', function(req, res) {
+  res.send('Welcome to the comments page!');
+});
+
+app.get('*', function(req, res) {
+  res.send('You are a star!!!');
+});
+
 app.listen(3000, function() {
   console.log('Server has started on port 3000');
 });
