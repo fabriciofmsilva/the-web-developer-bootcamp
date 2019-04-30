eb91161f061ff4c22980305228b8fc4c5c9585a2
Fabrício Silva <fabriciofmsilva@gmail.com>
2019-04-29 23:01:17 -0300

    EJS: Conditionals and Loops

--- 06-node/MoreExpress/app.js
+++ 06-node/MoreExpress/app.js
@@ -11,6 +11,15 @@ app.get('/fallinglovewith/:thing', function(req, res) {
   res.render('love.ejs', { thingVar: thing });
 });
 
+app.get('/posts', function(req, res) {
+  var posts = [
+    { title: 'Post 1', author: 'Susy' },
+    { title: 'My adorable pet bunny', author: 'Charlie' },
+    { title: 'Can you belive this pomsky?', author: 'Colt' },
+  ];
+  res.render('posts.ejs', { posts: posts });
+});
+
 app.listen(3000, function() {
   console.log('Server is listening!!!');
 });
