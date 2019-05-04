d744fab911d603010ebc1e2e0b2063a1dd931967
Fabrício Silva <fabriciofmsilva@gmail.com>
2019-05-04 15:05:10 -0300

    JSON Placeholder API Example

--- 06-node/APIs/first_request.js
+++ 06-node/APIs/first_request.js
@@ -1,9 +1,10 @@
-const request = require('request');
+const request = require('request-promise');
 
-console.log("Sunset in Hawaii is at...");
-request('https://query.yahooapis.com/v1/public/yql?q=select%20astronomy.sunset%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22maui%2C%20hi%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys', function(error, response, body){
-  if(!error && response.statusCode == 200){
-    const parsedData = JSON.parse(body);
-    console.log(parsedData["query"]["results"]["channel"]["astronomy"]["sunset"]);
-  }
-});
+request('https://jsonplaceholder.typicode.com/users/1')
+  .then((htmlstring) => {
+    const parsedData = JSON.parse(htmlstring);
+    console.log(`${parsedData.name} lives in ${parsedData.address.city}`);
+  })
+  .catch((error) => {
+    console.log('Error!', error);
+  });
