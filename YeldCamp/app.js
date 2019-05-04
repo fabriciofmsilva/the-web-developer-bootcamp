8240a43e36025181ffd0901e2fa5304749d64255
Fabrício Silva <fabriciofmsilva@gmail.com>
2019-05-04 18:51:02 -0300

    YelpCamp: Initial Routes

---
+++ YeldCamp/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+  res.render('landing');
+});
+
+app.get('/campgrounds', function(req, res) {
+  const campgrounds = [
+    {name: 'Salmon Creek', image: 'https://farm9.staticflickr.com/8167/7121865553_e1c6a31f07.jpg'},
+    {name: 'Granite Hill', image: 'https://farm6.staticflickr.com/5181/5641024448_04fefbb64d.jpg'},
+    {name: `Moutain Goat's Rest`, image: 'https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg'},
+  ];
+
+  res.render('campgrounds', { campgrounds });
+});
+
+app.listen(3000, function() {
+  console.log('The YelpCamp server has started!');
+});
