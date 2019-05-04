652105d5aa3e1d2c7d4f2bd44d0c55ac58e36427
Fabrício Silva <fabriciofmsilva@gmail.com>
2019-05-04 15:40:24 -0300

    Movie API App: Adding Search

--- 06-node/movie_search_app/app.js
+++ 06-node/movie_search_app/app.js
@@ -4,10 +4,18 @@ const request = require('request');
 
 app.set('view engine', 'ejs');
 
+app.get('/', function(req, res) {
+  res.render('search');
+});
+
 app.get('/results', function(req, res) {
-  request('http://www.omdbapi.com/?s=guardians+of+the+galaxy&apikey=thewdb', function(error, response, body) {
+  const query = req.query.search;
+  const url = `http://www.omdbapi.com/?s=${query}&apikey=thewdb`;
+
+  request(url, function(error, response, body) {
     if (!error && response.statusCode === 200) {
       const data = JSON.parse(body);
+      console.log(data);
       res.render('results', { data });
     }
   });
