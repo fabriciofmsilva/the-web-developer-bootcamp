0bd4fca88d12d3416b429f9f4a56cc3e322b777d
Fabrício Silva <fabriciofmsilva@gmail.com>
2019-05-04 14:48:11 -0300

    Making API Requests with Node

---
+++ 06-node/APIs/first_request.js
@@ -0,0 +1,6 @@
+var request = require('request');
+request('https://swapi.co/api/people/1', function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+});
